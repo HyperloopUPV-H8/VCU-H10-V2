4aa46148a9f499e24ffbbc5251d1fc377ccd19a5
Cantonplas <dcancat@etsinf.upv.es>
2025-06-06 23:20:14 +0200

    Codigo de pruebas(funcionan las ordenes)

--- Core/Src/Runes/generated_metadata.cpp
+++ Core/Src/Runes/generated_metadata.cpp
@@ -5,11 +5,11 @@
 extern "C"{
     const char DESCRIPTION[255]  __attribute__((section(".metadata_pool")))= 
         "****************"  // placeholder for beggining
-        "20250606T152735"   // DateTime using ISO-8601 format
+        "20250606T231945"   // DateTime using ISO-8601 format
         " "                 // alignment
         "7b1cff0f"          // STLIB commit
         "--------"          // ADJ commit
-        "74e2190b"          // Board commit
+        "64bec836"          // Board commit
         // the '=' is used for unparsing
         ;
 }
--- Core/Src/main.cpp
+++ Core/Src/main.cpp
@@ -6,22 +6,43 @@
 
 #include "ST-LIB.hpp"
 
-PWM Pump_c1(PE9);
-PWM Pump_c2(PE11);
+enum Pump{
+    PUMP_UNIDADES = 0,
+    PUMP_PLACAS = 1
+};
 
-PWM Regulator_out1(PB9);//por caracterizar
-PWM Regulator_out2(PD13);
+enum Regulator{
+    REGULATOR_1 = 0,
+    REGULATOR_2 = 1
+};
+
+
+
+//Potencia refri
+uint32_t potencia_unidades = 0;
+Pump selected_pump;
+
+PWM Pump_c1;
+PWM Pump_c2;
+
+
+//Regulator out
+float Selected_Regulator_pressure=0;//me falta 1 por hacer , los 2 el mismo??
+Regulator selected_regulator;
+
+PWM Regulator_out1;//por caracterizar
+PWM Regulator_out2;
 
 //Actuator:
 bool Activate_actuator = false;
 DigitalOutput Actuator_out(PG1);
 
-static void Potencia_unidades_callback() {// hay que caracterizar el pwm en estas 2
-    //Lo que haga el callback
-}
-
-static void Potencia_placas_callback() {
-    //Lo que haga el callback
+static void Pump_callback() {// hay que caracterizar el pwm en estas 2
+    if(selected_pump == Pump::PUMP_UNIDADES) {
+        Pump_c1.set_duty_cycle(potencia_unidades);
+    } else if (selected_pump == Pump::PUMP_PLACAS) {
+        Pump_c2.set_duty_cycle(potencia_unidades);
+    }
 }
 
 static void Activate_actuator_callback() {
@@ -33,8 +54,12 @@ static void Activate_actuator_callback() {
 }
 
 static void Set_regulator_callback() {
-    //Pasar a frecuencia y duty los porcentajes:
-    
+    float regulator_value = static_cast<float>(Selected_Regulator_pressure * (100.0 / 0.9));
+    if(selected_regulator == Regulator::REGULATOR_1){
+        Regulator_out1.set_duty_cycle(regulator_value);
+    }else if(selected_regulator == Regulator::REGULATOR_2){
+        Regulator_out2.set_duty_cycle(regulator_value);
+    }    
 }
 
 
@@ -43,95 +68,142 @@ int main(void) {
 #ifdef SIM_ON
     SharedMemory::start();
 #endif
+
+    Pump_c1=PWM(PE9);
+    Pump_c2=PWM(PE11);
+
+    Regulator_out1=PWM(PB9);//por caracterizar
+    Regulator_out2=PWM(PD13);
+
     //SDC_sense
-    DigitalInput SDC_sense;
-    SDC_sense.inscribe(PA12); 
+    PinState Sdc;
+    DigitalSensor SDC_sense(PA12,&Sdc); 
 
     //reeds
-    DigitalInput reed1_input;
-    DigitalInput reed2_input;
-    DigitalInput reed3_input;
-    DigitalInput reed4_input;
-    DigitalInput reed5_input;
-    DigitalInput reed6_input;
-    DigitalInput reed7_input;
-    DigitalInput reed8_input;
+    PinState reed1;
+    PinState reed2;
+    PinState reed3;
+    PinState reed4;
+    PinState reed5;
+    PinState reed6;
+    PinState reed7;
+    PinState reed8;
 
-    reed1_input.inscribe(PD11);
-    reed2_input.inscribe(PD10);
-    reed3_input.inscribe(PG4);
-    reed4_input.inscribe(PG3);
-    reed5_input.inscribe(PD8);
-    reed6_input.inscribe(PD9);
-    reed7_input.inscribe(PB15);
-    reed8_input.inscribe(PB14);
-
-    uint32_t reed1;
-    uint32_t reed2;
-    uint32_t reed3;
-    uint32_t reed4;
-    uint32_t reed5;
-    uint32_t reed6;
-    uint32_t reed7;
-
-    //Potencia refri
-    uint32_t potencia_unidades = 0;
-    uint32_t potencia_placas = 0;//Pasarlo a float?
+    DigitalSensor reed1_input(PD11,&reed1);
+    DigitalSensor reed2_input(PD10,&reed2);
+    DigitalSensor reed3_input(PG4,&reed3);
+    DigitalSensor reed4_input(PG3,&reed4);
+    DigitalSensor reed5_input(PD8,&reed5);
+    DigitalSensor reed6_input(PD9,&reed6);
+    DigitalSensor reed7_input(PB15,&reed7);
+    DigitalSensor reed8_input(PB14,&reed8);
 
     
+    
 
-    //Regulator out
-    uint32_t Selected_Regulator_pressure=0;//me falta 1 por hacer 
+    
    
     //in:
-    uint32_t regulator_1;
-    // uint32_t regulator_2;
-    DigitalInput Regulator_in1;
-    Regulator_in1.inscribe(PA4);
-    DigitalInput Regulator_in2;
-    Regulator_in2.inscribe(PD12);
+    PinState regulator_1;
+    PinState regulator_2;
+    DigitalSensor Regulator_in1(PA4,&regulator_1);
+    DigitalSensor Regulator_in2(PD12,&regulator_2);
+    // DigitalInput Regulator_in2;
+    // Regulator_in2.inscribe(PD12);
 
 
     
 
     //flow:
-    DigitalInput flow1_input;
-    DigitalInput flow2_input;
 
-    flow1_input.inscribe(PE13);
-    flow2_input.inscribe(PE14);
-    uint32_t flow1;
-    uint32_t flow2;
+    PinState flow1;
+    PinState flow2;
 
+
+    DigitalSensor flow1_input(PE13,&flow1);
+    DigitalSensor flow2_input(PE14,&flow2);
     
+    float pressure_1 = 0;
+    float pressure_2 = 0;
+    float pressure_3 = 0;
+    float pressure_4 = 0;
 
-    
+    uint8_t PresionAlta = ADC::inscribe(PB0);
+    uint8_t PresionRegulador = ADC::inscribe(PB1);
+    uint8_t PresionFrenos = ADC::inscribe(PF11);
+    uint8_t PresionCapsula = ADC::inscribe(PF12);
 
 
-
-    HeapOrder Potencia_Pump_Unidades{ static_cast<uint16_t>(173145),&Potencia_unidades_callback,&potencia_unidades};
-    HeapOrder Potencia_Pump_Placas{ static_cast<uint16_t>(17491),&Potencia_placas_callback,&potencia_placas};
-    HeapOrder Activate_actuator_order( static_cast<uint16_t>(84564),&Activate_actuator_callback,&Activate_actuator);
-    HeapOrder Set_regulator( static_cast<uint16_t>(957654),&Set_regulator_callback,&Selected_Regulator_pressure);
+    HeapOrder Potencia_Pump{ static_cast<uint16_t>(1731),&Pump_callback,&potencia_unidades,&selected_pump};
+    HeapOrder Activate_actuator_order( static_cast<uint16_t>(8456),&Activate_actuator_callback,&Activate_actuator);
+    HeapOrder Set_regulator( static_cast<uint16_t>(9576),&Set_regulator_callback,&Selected_Regulator_pressure,&selected_regulator);
 
     static const IPV4 VCU_IP{"192.168.1.3"};
     static const IPV4 control_station_ip{"192.168.0.9"};
-    static const MAC local_mac{"00:80:E1:11:02:00"};
-    static const uint16_t local_port{50500};//Habra que ver tambien los puertos
+    static const uint16_t local_port{50500};
     static const uint16_t controlstation_port{50555};
     static const uint16_t udp_controlstation_port{50400};
+
+    HeapPacket Reeds{3124,&reed1,&reed2,&reed3,&reed4,&reed5,&reed6,&reed7,&reed8};
+    HeapPacket flow(6457,&flow1,&flow2);
+    HeapPacket Regulator(1312,&regulator_1);
+    HeapPacket Pression(1312,&pressure_1,&pressure_2,&pressure_3,&pressure_4);
+
+    string mask,gateway,ip;
+    mask ="255.255.0.0";
+    gateway = "192.168.1.1";
+    ip = "192.168.1.3";
+
+
+    STLIB::start("00:80:e1:00:00:00",ip, mask, gateway, UART::uart2);
 
     ServerSocket Control_station(VCU_IP,local_port);
     DatagramSocket udp_controlstation(VCU_IP,udp_controlstation_port,control_station_ip,controlstation_port);
 
-    HeapPacket Reeds{31241,&reed1,&reed2,&reed3,&reed4,&reed5,&reed6,&reed7};
-    HeapPacket flow(64578,&flow1,&flow2);
-    HeapPacket Regulator(13124,&regulator_1);
+    Pump_c1.set_frequency(10000);
+    Pump_c1.set_duty_cycle(0); 
+    Pump_c1.turn_on();
 
+    Pump_c2.set_frequency(10000);
+    Pump_c2.set_duty_cycle(0); 
+    Pump_c2.turn_on();
 
+    Regulator_out1.set_frequency(10000);
+    Regulator_out1.set_duty_cycle(0);
+    Regulator_out1.turn_on();
 
-    STLIB::start();
+    Regulator_out2.set_frequency(10000);
+    Regulator_out2.set_duty_cycle(0);
+    Regulator_out2.turn_on();
 
+    Time::register_high_precision_alarm(1000, [&]() {
+        // SDC_sense.read();
+        
+        reed1_input.read();
+        reed2_input.read();
+        reed3_input.read();
+        reed4_input.read();
+        reed5_input.read();
+        reed6_input.read();
+        reed7_input.read();
+        reed8_input.read();
+
+        Regulator_in1.read();
+        Regulator_in2.read();
+
+        flow1_input.read();
+        flow2_input.read();
+
+        pressure_1 = ADC::get_int_value(PresionAlta);
+        pressure_2 = ADC::get_int_value(PresionRegulador);
+        pressure_3 = ADC::get_int_value(PresionFrenos);
+        pressure_4 = ADC::get_int_value(PresionCapsula);
+
+        udp_controlstation.send_packet(Reeds);
+        udp_controlstation.send_packet(flow);
+        udp_controlstation.send_packet(Regulator);
+
+    });
 
     while (1) {
         STLIB::update();
